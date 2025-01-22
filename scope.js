10ff6d91b4134b22931433b5419cc625c859a01f
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-22 11:31:45 +0530

    scopes

--- scope.js
+++ scope.js
@@ -1,26 +1,26 @@
 // globel scope
 
-// function one(){
-//     globel =10
-//     console.log("one",globel)
-// }
-// function two(){
-//     console.log("two",globel);
-// }
+function one(){
+    globel =10
+    console.log("one",globel)
+}
+function two(){
+    console.log("two",globel);
+}
 
 
-// one()
-// two()
+one()
+two()
 
 // local scope
 
-function one(num1,num2){
-    console.log("one",num1)
-}
+// function one(num1,num2){
+//     console.log("one",num1)
+// }
 
-function two(){
-    console.log("two",num1);
-}
+// function two(){
+//     console.log("two",num1);
+// }
 
-one(1)
-two(1)
+// one(1)
+// two(1)
