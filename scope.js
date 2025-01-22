2e1b131fe4bf9f76e475c312c097d72aa62cc188
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-22 11:29:40 +0530

    scope

---
+++ scope.js
@@ -0,0 +1,26 @@
+// globel scope
+
+// function one(){
+//     globel =10
+//     console.log("one",globel)
+// }
+// function two(){
+//     console.log("two",globel);
+// }
+
+
+// one()
+// two()
+
+// local scope
+
+function one(num1,num2){
+    console.log("one",num1)
+}
+
+function two(){
+    console.log("two",num1);
+}
+
+one(1)
+two(1)
--- spread operators.js
+++ spread operators.js
@@ -3,8 +3,16 @@
 const player =["faizy","fayiz","fasi","jasim","dilshad"]
 const player2 =["faiz","fayi","fas","jasi","dilsha"]
 
-const player1 = {...player}; // copy aan  eth
+const player1 = {...player};
 
 
 console.log(player);
 console.log(player2);
+
+// rest parameeter
+
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log(sum(1, 2, 3, 4));
