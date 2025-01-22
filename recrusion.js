df190404bdab405b407c017bb131ef0c2a09636a
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-22 11:14:51 +0530

    recrusion

--- recrusion.js
+++ recrusion.js
@@ -1,12 +1,19 @@
-// recrusion
-function factorial(n) {
-    if (n == 0) {
-        return 1;
+// function sum(n){
+//     let result = 0;
+//     for (let i= 1; i<=n ;i++){
+//         result += i;
+//     }
+//     return result;
+// }
+
+// console.log(sum(3));
+
+function sum(n){
+    if (n<=0){
+        return 0;
     } else {
-        return n * factorial(n - 1);
+        return n + sum (n-1)
     }
 }
 
-console.log(factorial(5));
-
-
+console.log(sum(3));
