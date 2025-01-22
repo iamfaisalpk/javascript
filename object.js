dae694cede9550ad53be52037a6b81c5d73350e2
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-22 14:27:45 +0530

    object

---
+++ object.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+    return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log(person.fullName());
+
+
