08eb0c9bf4f3ef87ac198c7766319785f768f337
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-23 14:32:31 +0530

    localstorage

---
+++ localstorage.js
@@ -0,0 +1,19 @@
+// localStorage.setItem("","")
+// localStorage.getItem("")
+// localStorage.removeItem("")
+// localStorage.clear("")
+// localStorage.key("")
+
+// const obj ={
+//     id :  0,
+//     name : "faizy"
+// };
+
+// localStorage.setItem("data",JSON.stringify(obj));
+
+// console.log(JSON.parse(localStorage.getItem("data")));
+
+localStorage.setItem("token","1");
+localStorage.setItem("name","faizy");
+
+console.log(localStorage.key(1));
